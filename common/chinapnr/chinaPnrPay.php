07d4a05ed8acc098ebdf3c9e6540b2ac53d35559
q920980002 <920980002@qq.com>
2016-01-22 11:39:58 +0800

    a

--- common/chinapnr/chinaPnrPay.php
+++ common/chinapnr/chinaPnrPay.php
@@ -793,7 +793,7 @@ EOf;
         }
 
 
-        error_log('【' . date("Y-m-d H:i:s") . '】 ' . $url . '?' . http_build_query($tem_arr) . "\n".iconv('GBK', 'UTF-8', $req_str), 3, dirname(__FILE__) . "/api_req.log");
+        error_log('【' . date("Y-m-d H:i:s") . '】\n' . $url . '?' . http_build_query($tem_arr) . "\n".iconv('GBK', 'UTF-8', $req_str). "\n\n\n", 3, dirname(__FILE__) . "/api_req.log");
 
         //请求数据
         $res = $this->_curl($url, $tem_arr);
